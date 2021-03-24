a7239626f128a9e55174a2c444b6a9e1069aac15
Noel Peterson <nmpeterson@users.noreply.github.com>
2021-03-24 11:31:58 -0500

    Corrected typos from Roxygen whitespace

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #' The official boundaries of the Chicago Community Areas. Obtained 3/24/2021.
 #'
 #' @format A multipolygon `sf` object with `r nrow(cca_sf)` rows and
-#'   `r ncol(cca_sf)` variables:
+#' `r ncol(cca_sf)` variables:
 #' \describe{
 #'   \item{cca_name}{Community area name. Character.}
 #'   \item{cca_num}{Numeric ID, as assigned by the City of Chicago. Integer.}
@@ -26,7 +26,7 @@
 #' 2015). Obtained 3/24/2021.
 #'
 #' @format A multipolygon `sf` object with `r nrow(ward_sf)` rows and
-#'   `r ncol(ward_sf)` variables:
+#' `r ncol(ward_sf)` variables:
 #' \describe{
 #'   \item{ward_num}{Ward number. Integer.}
 #' }
